284558ec24414a8263c755581dfead6de5c93814
Cindy <cindy@Students-MacBook-Pro.local>
2021-07-16 10:46:02 -0600

    complete objective2lab2

--- Labs/Objective2Lab2.java
+++ Labs/Objective2Lab2.java
@@ -1,10 +1,11 @@
 public class Objective2Lab2 {
   public static void main(String[] args) {
-    System.out.println("-----------------------");
-    System.out.println(" Grocery List: ");
-    System.out.println(" 1. Soy Milk");
-    System.out.println(" 2. Ginger Snaps");
-    System.out.println(" 3. Java Bean Monster");
-    System.out.println("-----------------------");
+    String name = "Cindy Chase";
+    int age = 29;
+    String state = "Colorado";
+
+    System.out.print("Name: " + name + " ");
+    System.out.print("Age: " + age + " ");
+    System.out.println("State: " + state);
   }
 }
