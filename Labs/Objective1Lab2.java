99b41a64a2a2bd634b3e135b290ef0fde4c7ae3a
Cindy <cindy@Students-MacBook-Pro.local>
2021-07-16 10:36:27 -0600

    complete objective1lab2

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    System.out.println("-----------------------");
+    System.out.println(" Grocery List: ");
+    System.out.println(" 1. Soy Milk");
+    System.out.println(" 2. Ginger Snaps");
+    System.out.println(" 3. Java Bean Monster");
+    System.out.println("-----------------------");
+  }
+}
