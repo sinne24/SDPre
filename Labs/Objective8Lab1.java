e602cfa155b82ee353cbcb3cbc73608e137cfad9
Cindy <cindy@Students-MacBook-Pro.local>
2021-07-16 17:04:17 -0600

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,13 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for(int i = 1; i < 10; i++){
+      System.out.println(i);
+    }
+
+    for(int i = 10; i > 0; i--){
+      System.out.println(i);
+    }
+
+
+  }
+}
