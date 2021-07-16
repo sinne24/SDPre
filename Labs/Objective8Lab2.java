bd6c8459faa637ab64cfff369f91976fba69157b
Cindy <cindy@Students-MacBook-Pro.local>
2021-07-16 17:11:13 -0600

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2{
+  public static void main(String[] args){
+    int sum = 0;
+
+    for(int i = 1; i < 21; i++){
+      sum = i + sum;
+    }
+
+    System.out.println(sum);
+  }
+}
