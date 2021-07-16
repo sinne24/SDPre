ab2cd8f86f1e8da50e91a8d9156b947ddc85712b
Cindy <cindy@Students-MacBook-Pro.local>
2021-07-16 10:52:04 -0600

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    num1 = 30;
+    num2 = 20;
+    num3 = 60;
+    average = (num1 + num2 + num3)/3;
+
+    System.out.println(average);
+  }
+}
